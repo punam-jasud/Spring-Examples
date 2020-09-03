0652c81597937aa83496261c799565a42b4d0234
Punam Jasud <punamj@synerzipune.local>
2020-09-03 18:24:11 +0530

    Dependency Injection By Constructor Example

---
+++ ConstructorInjection-Example/src/main/java/com/myproject/ConstructorInjection_Example/App.java
@@ -0,0 +1,14 @@
+package com.myproject.ConstructorInjection_Example;
+
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+
+public class App 
+{
+    public static void main( String[] args )
+    {
+        ApplicationContext context = new ClassPathXmlApplicationContext("Bean.xml");
+        Employee employee = (Employee)context.getBean("emp");
+        employee.displayDetails();
+    }
+}
