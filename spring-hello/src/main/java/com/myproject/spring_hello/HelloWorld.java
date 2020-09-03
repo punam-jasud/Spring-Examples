f74411c018b850aa1f6f0d49dee2a6840890adb3
Punam Jasud <punamj@synerzipune.local>
2020-09-03 14:38:13 +0530

    Hello World in Spring

---
+++ spring-hello/src/main/java/com/myproject/spring_hello/HelloWorld.java
@@ -0,0 +1,15 @@
+package com.myproject.spring_hello;
+
+public class HelloWorld {
+	
+	private String Message;
+
+	public void setMessage(String message) {
+		Message = message;
+	}
+	
+	public String getMessage() {
+		return Message;
+	}
+
+}
