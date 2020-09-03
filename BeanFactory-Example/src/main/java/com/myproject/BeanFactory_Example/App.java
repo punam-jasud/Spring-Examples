25f0d85a8775833517f8022a886bfef3bf2681e6
Punam Jasud <punamj@synerzipune.local>
2020-09-03 15:07:56 +0530

    ApplicationContext Container Example in Spring

--- BeanFactory-Example/src/main/java/com/myproject/BeanFactory_Example/App.java
+++ BeanFactory-Example/src/main/java/com/myproject/BeanFactory_Example/App.java
@@ -5,10 +5,6 @@ import org.springframework.beans.factory.xml.XmlBeanFactory;
 import org.springframework.core.io.ClassPathResource;
 import org.springframework.core.io.Resource;
 
-/**
- * Hello world!
- *
- */
 public class App 
 {
     public static void main( String[] args )
